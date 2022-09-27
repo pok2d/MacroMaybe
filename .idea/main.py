a936085ac4e3d057ef15cc3cf21ef6a668aa3c52
pok2d <113487242+pok2d@users.noreply.github.com>
2022-09-27 19:51:32 -0400

    Update1
    Just putting initial code in.

---
+++ .idea/main.py
@@ -0,0 +1,25 @@
+from pynput import keyboard
+
+currenlogpress = ''
+with keyboard.Events() as events:
+    for event in events:
+        if event.key == keyboard.Key.esc:
+            print(currenlogpress)
+            break
+        else:
+            curletter = str(format(event))
+            if curletter.find('P') == 0:
+                if curletter.find("'") == 10:
+                    currenlogpress += curletter[11]
+                    print('Letter added')
+                elif curletter[14] == 's':
+                    if curletter[15] == 'p':
+                        currenlogpress += ' '
+                        print('Space')
+                elif curletter[14] == 'b':
+                    if curletter[15] == 'a':
+                        currenlogpress = currenlogpress.rstrip(currenlogpress[-1])
+                        print('Backspace')
+
+
+
